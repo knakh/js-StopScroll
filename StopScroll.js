985280f94d583cf595687a98dc59ca92df65f456
knakh <very.tasty.konjac@gmail.com>
2019-02-25 17:50:34 +0900

    release

---
+++ StopScroll.js
@@ -0,0 +1,41 @@
+;'use strict';
+/**
+ * StopScroll
+ */
+var StopScroll = function () {
+  var t = this;
+  t.container = $(window);
+  t.concrete = $('body');
+  t.original = {
+    scrollTop: 0,
+  };
+  t.bindClassName = 'js-stop-scroll';
+  t.lockClassName = 'js-stop-scroll--locked';
+  t.concrete.addClass(this.bindClassName);
+};
+StopScroll.prototype = {
+  lockScroll: function () {
+    var t = this;
+
+    t.saveScrollTop();
+    t.concrete.addClass(t.lockClassName);
+    t.concrete.css('top', -t.original.scrollTop + 'px');
+  },
+  unlockScroll: function () {
+    var t = this;
+
+    t.concrete.removeClass(t.lockClassName);
+    t.container.scrollTop(t.original.scrollTop);
+  },
+  unbindClass: function () {
+    var t = this;
+
+    t.concrete.removeClass(t.bindClassName);
+  },
+  saveScrollTop: function () {
+    var t = this;
+
+    t.original.scrollTop = t.container.scrollTop();
+  },
+};
+
