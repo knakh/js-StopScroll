af0d9e377a4c55bb92421a01bd9bd516bd33cfcd
Kohey <very.tasty.konjac@gmail.com>
2020-08-04 17:23:06 +0900

    update.

---
+++ StopScrolling.js
@@ -0,0 +1,33 @@
+;'use strict';
+/**
+ * StopScrolling
+ */
+var StopScrolling = function () {
+  var _this = this;
+  _this.container = $(window);
+  _this.concrete = $('body');
+  _this.original = {
+    scrollTop: 0,
+  };
+  _this.lockClassName = 'js-stop-scrolling';
+};
+StopScrolling.prototype = {
+  stop: function () {
+    var _this = this;
+
+    _this.saveScrollTop();
+    _this.concrete.addClass(_this.lockClassName);
+    _this.concrete.css('top', -_this.original.scrollTop + 'px');
+  },
+  purge: function () {
+    var _this = this;
+
+    _this.concrete.removeClass(_this.lockClassName);
+    _this.container.scrollTop(_this.original.scrollTop);
+  },
+  saveScrollTop: function () {
+    var _this = this;
+
+    _this.original.scrollTop = _this.container.scrollTop();
+  },
+};
